3a311d0406cb15cd5d4834914fa6c5e820a61c34
aayush <aayushkothari11@yahoo.com>
2019-03-02 15:18:51 +0530

    small typo

--- OSL/urls.py
+++ OSL/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
