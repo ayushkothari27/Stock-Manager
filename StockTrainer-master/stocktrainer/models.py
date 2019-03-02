206f51a76a854787496097ef7adb0364fd883695
fenil-25 <fenildanbrown@gmail.com>
2019-03-02 16:21:12 +0530

    Models updated

--- StockTrainer-master/stocktrainer/models.py
+++ StockTrainer-master/stocktrainer/models.py
@@ -13,16 +13,22 @@ class Crypto(models.Model):
 class Stock(models.Model):
     name = models.CharField(max_length=100)
     symbol = models.CharField(max_length=10, blank=True,default="")
-    website = models.CharField(max_length=500, default="")
-    sector = models.CharField(max_length=500, default="")
-    industry = models.CharField(max_length=500, default="")
-    last_annual_total_assets = models.BigIntegerField(default=0)
-    last_annual_revenue = models.BigIntegerField(default=0)
-    last_annual_net_income = models.BigIntegerField(default=0)
-    last_annual_eps = models.FloatField(default=0)
+    price = models.CharField(max_length=12, blank=True)
+    region = models.CharField(max_length=30, blank=True)
+    history = models.TextField(blank=True, default="NA")
+    prediction = models.TextField(blank=True, default="NA")
+    sentiment = models.CharField(max_length=50, blank=True, default="NA")
+
+    # website = models.CharField(max_length=500, default="")
+    # sector = models.CharField(max_length=500, default="")
+    # industry = models.CharField(max_length=500, default="")
+    # last_annual_total_assets = models.BigIntegerField(default=0)
+    # last_annual_revenue = models.BigIntegerField(default=0)
+    # last_annual_net_income = models.BigIntegerField(default=0)
+    # last_annual_eps = models.FloatField(default=0)
 
     def __str__(self):
-        return self.name
+        return str(self.name) + ' (' + str(self.symbol) + ')'
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
@@ -35,11 +41,11 @@ class Watch(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='entries')
     stock = models.ForeignKey(Stock, on_delete=models.CASCADE, related_name='stock')
     added_on = models.DateField(("Date"), default=datetime.date.today, blank=True)
-    
+
 
     def __str__(self):
         return self.stock.name
-        
+
 
 class Buy(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bentries')
